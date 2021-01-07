aff36c49b806cda3ac406c9ea1b48544167cfb5b
Pratik Bankar <blakleaf.003@outlook.com>
2021-01-08 02:09:12 +0530

    Initial look of our Products which needs a lot of styling to have a lookup properly

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,109 @@ import React from 'react'
 import Banner from './Banner'
 import './Home.css'
 import Product from './Product'
+import Avalon from './images/avalon.jpg'
 
 function Home() {
     return (
         <div className='home'>
             <Banner className='banner_writing' />
             <div className="home_items">
-                <Product />
+                <div className="home_itemsRow">
+                    <Product
+                        id='1'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='2'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='3'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='4'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='5'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='6'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                </div>
 
+                <div className="home_itemsRow">
+                    <Product
+                        id='1'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='2'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='3'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                </div>
+
+                <div className="home_itemsRow">
+                    <Product
+                        id='1'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                    <Product
+                        id='2'
+                        title='Starship Avalon'
+                        price={100000000}
+                        image={Avalon}
+                        rating={10}
+                        placeholder='Starship Avalon'
+                    />
+                </div>
             </div>
 
 
--- src/Product.js
+++ src/Product.js
@@ -3,22 +3,24 @@ import './Product.css'
 import Star from './images/star.svg'
 import Avalon from './images/avalon.jpg'
 
-function Product() {
+function Product({ id, title, image, price, rating, placeholder }) {
     return (
         <div className='product'>
             <div className="product_info">
-                <p>Starship Avalon</p>
+                <p>{title}</p>
                 <p className="product_price">
                     <small>$</small>
-                    <strong>1M</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product_rating">
-                    <p><img className='star' src={Star} alt="Star Rating" /></p>
-                    <p><img className='star' src={Star} alt="Star Rating" /></p>
-                    <p><img className='star' src={Star} alt="Star Rating" /></p>
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p><img className='star' src={Star} alt="Star Rating" /></p>
+                        ))}
                 </div>
             </div>
-            <img className='product_image' src={Avalon} alt="Starship Avalon model 1" />
+            <img className='product_image' src={image} alt={placeholder} />
             <button>Add to Cart</button>
         </div>
     )
