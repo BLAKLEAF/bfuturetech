d5bf3a168f3f6edb0c0f419bbae287fbd321f963
Pratik Bankar <blakleaf.003@outlook.com>
2021-01-10 00:39:10 +0530

    forgot to add Checkout component files in staging area

---
+++ src/Checkout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Checkout.css'
+
+function Checkout() {
+    return (
+        <div className='checkout'>
+            <div className="checkout_left">
+                <h2 className="checkout_title">
+                    Your cart items
+                </h2>
+
+            </div>
+            <div className="checkout_right">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
