3b1e8a9275f2ab8df6005142abf41c328bb33ffa
Pratik Bankar <blakleaf.003@outlook.com>
2021-01-13 00:49:29 +0530

    added payment page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Checkout from './Checkout';
 import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import Payment from "./Payment";
 
 function App() {
   const [{ }, dispatch] = useStateValue();
@@ -51,7 +52,7 @@ function App() {
           <Route path='/payment'>
             <Header />
             <Banner />
-
+            <Payment />
           </Route>
           <Route path='/'>
             <Header />
