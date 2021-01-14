95f97bbf58740e418264c4c013a50b99f4c877b9
Pratik Bankar <blakleaf.003@outlook.com>
2021-01-14 23:33:23 +0530

    added Orders and Order component

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,54 @@
 const functions = require('firebase-functions');
 
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51I8srBEGXsNNlzr9YDSeZHe97USpX4vaZCUS2fvdIbAIcgGqNbqSXrv0rUeISEylZFW4gIVDQeKbzu3mxVlcgntc005qjxyrzH');
+
+// API
+
+// App Config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API Routes
+app.get('/', (request, response) => response.status(200).send('hello world'));
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+    });
+
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+// Listen Command
+exports.api = functions.https.onRequest(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
